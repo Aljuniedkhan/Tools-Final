430106ef5434557eb742c38e837f09a4ee7e91f6
aljuniedkhan <juniedk54@gmail.com>
2025-01-17 11:12:23 +0600

    bottons are added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import './App.css'
 function App() {
 
   return (
-    <div className='flex justify-between'>
-      <h1 className='text-3xl font-bold'><span className='text-red-600'>PH</span>Tube</h1>
-      <button className="btn btn-neutral">Sort by View</button>
-      <button className="btn btn-error">Blog</button>
-
-    </div>
+    <>
+      <div className='flex justify-between'>
+        <h1 className='text-3xl font-bold'><span className='text-red-600'>PH</span>Tube</h1>
+        <button className="btn btn-neutral">Sort by View</button>
+        <button className="btn btn-error">Blog</button>
+      </div>
+      <div className='flex gap-x-4 justify-center align-center mt-8'>
+      <button className="btn btn-error">All</button>
+      <button className="btn btn-outline">Music</button>
+      <button className="btn btn-outline">Comedy</button>
+      <button className="btn btn-outline">Drawing</button>
+      </div>
+    </>
   )
 }
 
